a491c8feb0c6d848c85cd4ae6acb5e0dc93b696a
svitlana.oliinyk <101010916+oliinyks@users.noreply.github.com>
2023-03-16 19:13:36 +0200

    add logic

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-	const [followers, setFollowers] = useState('');
+  const [followers, setFollowers] = useState(100500);
+  const [isFollow, setIsFollow] = useState(false);
+
+  const onBtnClick = (e) => {
+    if (isFollow) {
+      setFollowers((prev) => (prev -= 1));
+    } else {
+      setFollowers((prev) => (prev += 1));
+    }
+    setIsFollow((current) => !current);
+  };
 
   return (
     <div className="app">
@@ -20,8 +30,16 @@ function App() {
         <img className="user-photo" src={user} alt="user" />
         <div className="user-content">
           <p className="user-tweets">777 tweets</p>
-          <p className="user-followers">100,501 followers</p>
-          <button className="user-button">following</button>
+          <p className="user-followers">
+            {followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+            followers
+          </p>
+          <button
+            onClick={onBtnClick}
+            className={isFollow ? "user-button button-active" : "user-button"}
+          >
+            {isFollow ? "following" : "follow"}
+          </button>
         </div>
       </main>
     </div>
